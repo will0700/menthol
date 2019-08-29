7f66d59254849041d6d6975c9f0f9dca39b58c29
will <will0700@gmail.com>
2019-08-28 20:59:45 -0700

    fixed urls and models 8:59pm

--- apps/menthol_app/models.py
+++ apps/menthol_app/models.py
@@ -3,7 +3,7 @@ from apps.login_reg.models import *
 
 class Account(models.Model):
     name = models.CharField(max_length=45) #ex. BoA Checking, Chase CC, etc
-    acc_balance = models.DecimalField(decimal_places=2)
+    acc_balance = models.DecimalField(decimal_places=2, max_digits=12)
     category = models.CharField(max_length=45) #only checking, saving, credit_card |for now|Venmo as reach goal|
     owner = models.ForeignKey(User, related_name="accounts")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,8 +11,8 @@ class Account(models.Model):
 
 class Expense(models.Model):
     name = models.CharField(max_length=45) #ex. Food, Gas, Shopping, etc
-    exp_balance = models.DecimalField(decimal_places=2)
-    budget = models.DecimalField(decimal_places=2)
+    exp_balance = models.DecimalField(decimal_places=2, max_digits=12)
+    budget = models.DecimalField(decimal_places=2, max_digits=12)
     owner = models.ForeignKey(User, related_name="expenses")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -20,20 +20,20 @@ class Expense(models.Model):
 class Payment(models.Model):
     vendor = models.CharField(max_length=45) #ex. Taco Bell, Nordstrom, etc
     description = models.CharField(max_length=45) #ex. New red flannel, lunch with Eduardo, etc
-    payment_amount = models.DecimalField(decimal_places=2)
+    payment_amount = models.DecimalField(decimal_places=2, max_digits=12)
     debit_exp = models.ForeignKey(Expense, related_name="payments_debit")
     credit_acc = models.ForeignKey(Account, related_name="payments_credit")
-    owner = models.ForeignKey(User, related_name="expenses")
+    owner = models.ForeignKey(User, related_name="payments")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Transfer(models.Model):
     vendor = models.CharField(max_length=45) #ex. Taco Bell, Nordstrom, Chase Bank, etc
     description = models.CharField(max_length=45) #ex. Pay Chase Card off with BoA Checking, Buy Gas at Arco with Debit Card, etc
-    transfer_amount = models.DecimalField(decimal_places=2)
+    transfer_amount = models.DecimalField(decimal_places=2, max_digits=12)
     debit_acc = models.ForeignKey(Account, related_name="transfers_debit")
     credit_acc = models.ForeignKey(Account, related_name="transfers_credit")
-    owner = models.ForeignKey(User, related_name="expenses")
+    owner = models.ForeignKey(User, related_name="transfers")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
